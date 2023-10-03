1ca0c3fae5a5152004f2d571ccd077b2b3854624
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-03 16:22:52 +0100

    Commenting and small tweaks

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
+// Toggles between dark and lightmode using the bootstrap attribute 
 let darkBut = document.getElementById("dark-mode-toggle");
-let htmlRoot = document.getElementById("htmlRoot")
+let htmlRoot = document.getElementById("htmlRoot");
 darkBut.addEventListener("click", (e) => {
-    console.log("Changed colour mode")
+    console.log("Changed colour mode");
     if (htmlRoot.getAttribute("data-bs-theme")==="dark"){
-        htmlRoot.setAttribute("data-bs-theme", "light")
+        htmlRoot.setAttribute("data-bs-theme", "light");
     }
     else{
-        htmlRoot.setAttribute("data-bs-theme", "dark")
+        htmlRoot.setAttribute("data-bs-theme", "dark");
     }
     
 })
+
+console.log($);
