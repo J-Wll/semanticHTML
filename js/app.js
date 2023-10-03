bd8c94edf18efd87db04ee727699c2ef3b5891ec
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-03 13:02:39 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let darkBut = document.getElementById("dark-mode-toggle");
+let htmlRoot = document.getElementById("htmlRoot")
+darkBut.addEventListener("click", (e) => {
+    console.log("Changed colour mode")
+    if (htmlRoot.getAttribute("data-bs-theme")==="dark"){
+        htmlRoot.setAttribute("data-bs-theme", "light")
+    }
+    else{
+        htmlRoot.setAttribute("data-bs-theme", "dark")
+    }
+    
+})
