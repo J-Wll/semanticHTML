34d425a7d7f564bae9512c22a556f2ec4484f53c
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-03 17:31:01 +0100

    Formatting

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
+console.log($);
 // Toggles between dark and lightmode using the bootstrap attribute 
 let darkBut = document.getElementById("dark-mode-toggle");
 let htmlRoot = document.getElementById("htmlRoot");
 darkBut.addEventListener("click", (e) => {
     console.log("Changed colour mode");
-    if (htmlRoot.getAttribute("data-bs-theme")==="dark"){
+    if (htmlRoot.getAttribute("data-bs-theme") === "dark") {
         htmlRoot.setAttribute("data-bs-theme", "light");
     }
-    else{
+    else {
         htmlRoot.setAttribute("data-bs-theme", "dark");
     }
-    
-})
 
-console.log($);
+})
